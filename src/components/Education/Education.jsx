557a604c8373cbc5716e6b2ae80ac58635b16839
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-09-13 11:10:05 +0300

    fix education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,11 +3,17 @@
 const Education = () => {
     return (
         <>
+            <h2>EDUCATION</h2>
             <h3>IT School GoIT</h3>
-            <div>
-                <p>2023-2024</p>
-                <p>Fullstack Developer</p>
-            </div>
+
+            <p>2023-2024</p>
+            <p>Fullstack Developer</p>
+
+            <h3>Makeyevka Construction College</h3>
+
+            <p>1995-1998</p>
+            <p>Construction-Technician-Technologist</p>
+
         </>
 
     )
